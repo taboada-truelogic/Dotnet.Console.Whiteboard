bb3f6df6d31726ca6cf0d17414d2627c7d7579eb
Hernan Taboada <htaboada@truelogic.io>
2024-03-01 21:58:32 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Dotnet.Console.Whiteboard;
+
+using System;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+    }
+
+    public static bool Is24HourClock(string hoursMinutes)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Whiteboard
+
+## Problem
+
+Given a `string` in _hours:minutes_ format, check if it’s a correct representation of a 24-hour clock.
+
+## Example
+
+| Input  | Output |
+| ------ | ------ |
+| 13:37  | True   |
+| 25.52  | False  |
